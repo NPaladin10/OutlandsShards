e3a17773b51ecfebec9557c2f32005fb9fa51b00
NPaladin10 <sustainable.cycle@gmail.com>
2019-06-18 15:07:00 -0500

    V0.2.0

--- contracts/PlaneTap.sol
+++ contracts/PlaneTap.sol
@@ -1,63 +1,69 @@
 pragma solidity ^0.5.0;
 
-interface TokenRegistry {
-    function Token(uint256 _i) external view returns(address);
+interface IPlane {
+    function planeToToken(uint256 pi, uint256 si) external view returns(address, uint256);
+}
+
+interface Registry {
+    function simpleMintToken(uint256 _i, address _who, uint256 _amt) external;
+    function isMinter(address minter) external returns(bool);
+    function renounceMinter() external;
 }
 
 interface PlaneGen {
     function CPX(uint256 pi, uint256 si) external view returns(uint256[3] memory colors, uint256[3] memory mag);
 }
 
-interface OutlandsPlanes {
-    function planeFinder(uint256 pi, uint256 si) external view returns(address);
-}
-
-interface ERC777 {
-    function mint(address operator,
-        address account,
-        uint256 amount,
-        bytes calldata userData,
-        bytes calldata operatorData) external;
-}
+/**
+ * @dev Tap Planes for CPX  
+ * 
+ * Ropsten - 0xC285A7373C3C84C7dA9f97C161f5292fb62C9939
+ */
 
 contract PlaneTap {
-    address owner;
+    address payable owner;
+    IPlane Planes;
     PlaneGen Gen;
-    TokenRegistry TR;
-    OutlandsPlanes P;
-    
+    Registry TR;
+
     uint256 public timeBetweenTaps;
     mapping (uint256 => mapping(uint256 => uint256)) public nextTap;
     
-    constructor (address _p, address _gen, address _tr) public {
+    constructor () public {
         owner = msg.sender;
         //set the interface contracts 
-        Gen = PlaneGen(_gen);
-        TR = TokenRegistry(_tr);
-        P = OutlandsPlanes(_p);
+        Gen = PlaneGen(0xBC4e464f489c74978b8DeF438D37a62236A26add);
+        TR = Registry(0x5BB1a3E7ED4566aE9Ac02372bdD777245A6CcBa5);
+        Planes = IPlane(0x53CC757F23d3DC78fAA5b4D0B807e8cD3d548057);
     }
     
-    function _token(uint256 _i) internal view returns(address) {
-        return TR.Token(_i);
+    function kill () public {
+        require(msg.sender == owner);
+        //renounce minter 
+        if(TR.isMinter(address(this))) TR.renounceMinter();
+        //destroy
+        selfdestruct(owner);
     }
 
     function _mint(address _finder, address _who, uint256 _color, uint256 _val) internal {
         uint256 maxVal = 1 ether * _val / 100;
-        address t = _token(_color);
-        bytes memory nb;
         //The one who taps gets 89%, the finder gets 10%
-        ERC777(t).mint(t, _who, maxVal * 89/100, nb, nb);
-        ERC777(t).mint(t, _finder, maxVal * 10/100, nb, nb);
-        ERC777(t).mint(t, owner, maxVal * 1/100, nb, nb);
+        TR.simpleMintToken(_color, _who, maxVal * 89/100);
+        TR.simpleMintToken(_color, _finder, maxVal * 10/100);
+        TR.simpleMintToken(_color, owner, maxVal * 1/100);
     }
 
     function tap(uint256 pi, uint256 si) public {
+        //finder 
+        uint256 tid;
+        address finder;
+        (finder, tid) = Planes.planeToToken(pi, si);
+        //must be found 
+        require(finder != address(0));
         //cannot claim too quickly 
         require(nextTap[pi][si] < now);
         //update time 
         nextTap[pi][si] = now + timeBetweenTaps;
-        //finder 
-        address finder = P.planeFinder(pi, si);
         //get cosmic 
         uint256[3] memory colors;
         uint256[3] memory vals;
