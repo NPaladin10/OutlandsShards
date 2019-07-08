30bf9e360627e917871ffe996c6a67e76f303e3d
NPaladin10 <sustainable.cycle@gmail.com>
2019-07-08 14:54:05 -0500

    V0.3.0

--- contracts/OutlandsTrouble.sol
+++ contracts/OutlandsTrouble.sol
@@ -5,7 +5,7 @@ import "github.com/NPaladin10/OutlandsShards/contracts/OutlandsHeroes721.sol";
 import "github.com/NPaladin10/OutlandsShards/contracts/OutlandsXP.sol";
 
 /**
- * ropsten - 0xF4bE45F3d3d3042dd0ebB495eE9173632d3f183a
+ * ropsten - 
  */
 
 contract OutlandsTrouble is MinterRole{
@@ -17,9 +17,10 @@ contract OutlandsTrouble is MinterRole{
     OutlandsHero721 OH;
     OutlandsXP XP;
     //map when heroes were last committed
-    mapping (uint256 => uint256) internal _lastPeriod;
+    mapping (uint256 => uint256) public cooldown;
     //track period 
     uint256 public currentPeriod;
+    uint256 public timeBetweenPeriods = 60*60*24;
 
     //structure of challenge
     struct Challenge {
@@ -28,20 +29,24 @@ contract OutlandsTrouble is MinterRole{
         uint256[] heroes;
     }
     //listing of active challenges 
-    Challenge[] activeChallenges;
+    Challenge[] internal _activeChallenges;
     
     //core data 
     address admin;
     address payable bank;
-    uint256 costToChallenge = 1 finney;
+    uint256 public costToChallenge = 1 finney;
     
     constructor() public {
         admin = msg.sender;
-        bank = msg.sender;
+        bank = 0xB62cCa4D5982D52dff6043fCab8DEBe2bbaBf6AA;
+        //time 
+        currentPeriod = 1;
         //set up addresses
         XP = OutlandsXP(0x58E2671A70F57C1A76362c5269E3b1fD426f43a9);
         OH = OutlandsHero721(0xeBEF6F1ffc0c97a83FB136F9D45f81a6E471B4B8);
         CPX = CosmicTokenOperator(0x61a89f29cDeEBd7fdBC8c2d84Cd21f2c8aAd88e4);
+        //add MinterRole
+        addMinter(0xB62cCa4D5982D52dff6043fCab8DEBe2bbaBf6AA);
     }
 
     /**
@@ -49,13 +54,15 @@ contract OutlandsTrouble is MinterRole{
      * Heroes can only be used in one challenge per period
      * Heroes must be owned by the player 
      */
-    function _canChallenge (address player, uint256[] memory heroes) internal {
-        for(uint256 i = 0; i < 6; i++){
-            require(player == OH.ownerOf(heroes[i]));
-            require(_lastPeriod[heroes[i]] < currentPeriod);
-            //update last active period
-            _lastPeriod[heroes[i]] = currentPeriod;
+    function _canChallenge (address player, uint256[] memory heroes) internal view returns(bool){
+        uint256 hi;
+        uint256 n = heroes.length;
+        for(uint256 i = 0; i < n; i++){
+            hi = heroes[i];
+            require(player == OH.ownerOf(hi));
+            require(cooldown[hi] <= currentPeriod);
         }
+        return true;
     }
     
     /**
@@ -64,6 +71,9 @@ contract OutlandsTrouble is MinterRole{
      */
     function kill () public {
         require(msg.sender == admin);
+        //renounce  minter
+        CPX.renounceMinter();
+        XP.renounceMinter();
         //destroy
         selfdestruct(bank);
     }
@@ -83,14 +93,38 @@ contract OutlandsTrouble is MinterRole{
         require(msg.sender == admin);
         costToChallenge = cost;
     }
+    
+    /**
+     * @dev Set Time 
+     */
+    function setTimeBetweenPeriods(uint256 time) public {
+        require(msg.sender == admin);
+        timeBetweenPeriods = time;
+    }
+    
+    /**
+     * @dev View the number of Challenges 
+     */
+    function countOfChallenges() public view returns (uint256){
+        return _activeChallenges.length;
+    }
+    
+    /**
+     * @dev View a particular challenge
+     */
+    function getChallengeById(uint256 id) public view returns (address, uint256, uint256[] memory){
+        require(id < _activeChallenges.length);
+        Challenge memory aC = _activeChallenges[id];
+        return (aC.player, aC.tid, aC.heroes);
+    }
 
     /**
      * @dev Pushes next period 
      * Resets challenges and updates current period 
      */
     function nextPeriod() public onlyMinter {
-        delete activeChallenges;
-        currentPeriod++;
+        delete _activeChallenges;
+        currentPeriod = currentPeriod+1;
     }
     
     /**
@@ -104,7 +138,7 @@ contract OutlandsTrouble is MinterRole{
         uint256 n = ids.length;
         Challenge memory C;
         for(uint256 i = 0 ; i < n; i++){
-            C = activeChallenges[ids[i]];
+            C = _activeChallenges[ids[i]];
             //give xp
             for(uint256 j = 0 ; j < n; j++){
                 XP.giveXPSingle(C.heroes[j], xp[i][j]);
@@ -119,11 +153,11 @@ contract OutlandsTrouble is MinterRole{
     /**
      * @dev Adds extra cooldown phases to a hero 
      */
-    function addCooldown(uint256[] memory hi, uint256[] memory cool) public onlyMinter {
+    function setCooldown(uint256[] memory hi, uint256[] memory cool) public onlyMinter {
         require(hi.length == cool.length);
         uint256 n = hi.length;
         for(uint256 i = 0; i < n; i++){
-            _lastPeriod[hi[i]] += cool[i];
+            cooldown[hi[i]] = cool[i];
         }
     }
 
@@ -132,15 +166,20 @@ contract OutlandsTrouble is MinterRole{
      * Heroes can only be used in one challenge per period
      */
     function submitChallenge(uint256 ti, uint256[] memory heroes) public payable {
+        uint256 n = 6;
         require(msg.value >= costToChallenge);
-        require(heroes.length == 6);
+        require(heroes.length == n);
         //pay bank
         bank.transfer(address(this).balance);
         //validates heroes 
         _canChallenge(msg.sender, heroes);
+        //update cooldown
+        for(uint256 i = 0; i < n; i++){
+            cooldown[heroes[i]] = currentPeriod + 1;
+        }
         //creates a new challenge 
         Challenge memory tC = Challenge(msg.sender, ti, heroes);
-        activeChallenges.push(tC);
+        _activeChallenges.push(tC);
         //Challenge made
         emit NewChallenge(currentPeriod, msg.sender, ti);
     }
