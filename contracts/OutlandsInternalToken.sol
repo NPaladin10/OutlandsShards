0175b42ae73eaefe0f2fe06683110786a3b17d48
NPaladin10 <sustainable.cycle@gmail.com>
2019-07-24 15:12:46 -0500

    V0.0.2

---
+++ contracts/OutlandsInternalToken.sol
@@ -0,0 +1,187 @@
+pragma solidity ^0.5.0;
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
+
+/**
+* 
+*/
+
+contract OutlandsInternalToken is MinterRole
+{
+    using SafeMath for uint256;
+    
+    /**
+        @dev Either `TransferSingle` or `TransferBatch` MUST emit when tokens are transferred, including zero value transfers as well as minting or burning (see "Safe Transfer Rules" section of the standard).
+        The `_operator` argument MUST be msg.sender.
+        The `_from` argument MUST be the address of the holder whose balance is decreased.
+        The `_to` argument MUST be the address of the recipient whose balance is increased.
+        The `_id` argument MUST be the token type being transferred.
+        The `_value` argument MUST be the number of tokens the holder balance is decreased by and match what the recipient balance is increased by.
+        When minting/creating tokens, the `_from` argument MUST be set to `0x0` (i.e. zero address).
+        When burning/destroying tokens, the `_to` argument MUST be set to `0x0` (i.e. zero address).
+    */
+    event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _value);
+
+    /**
+        @dev Either `TransferSingle` or `TransferBatch` MUST emit when tokens are transferred, including zero value transfers as well as minting or burning (see "Safe Transfer Rules" section of the standard).
+        The `_operator` argument MUST be msg.sender.
+        The `_from` argument MUST be the address of the holder whose balance is decreased.
+        The `_to` argument MUST be the address of the recipient whose balance is increased.
+        The `_ids` argument MUST be the list of tokens being transferred.
+        The `_values` argument MUST be the list of number of tokens (matching the list and order of tokens specified in _ids) the holder balance is decreased by and match what the recipient balance is increased by.
+        When minting/creating tokens, the `_from` argument MUST be set to `0x0` (i.e. zero address).
+        When burning/destroying tokens, the `_to` argument MUST be set to `0x0` (i.e. zero address).
+    */
+    event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _ids, uint256[] _values);
+    
+    /**
+        @dev MUST emit when the URI is updated for a token ID.
+        URIs are defined in RFC 3986.
+        The URI MUST point a JSON file that conforms to the "ERC-1155 Metadata URI JSON Schema".
+    */
+    event URI(string _value, uint256 indexed _id);
+    
+    // id => (owner => balance)
+    mapping (uint256 => mapping(address => uint256)) internal balances;
+    
+    uint256 nonce;
+    address admin;
+    //contrtuctor
+    constructor () public {
+        admin = msg.sender;
+    }
+
+/////////////////////////////////////////// ERC1155 //////////////////////////////////////////////
+    // This function only creates the type.
+    function create(string calldata _uri)
+    external onlyMinter returns(uint256 _type) {
+        // Store the type in the upper 128 bits
+        _type = ++nonce;
+
+        // emit a Transfer event with Create semantic to help with discovery.
+        emit TransferSingle(msg.sender, address(0x0), address(0x0), _type, 0);
+
+        if (bytes(_uri).length > 0)
+            emit URI(_uri, _type);
+    }
+    
+    //mint token 
+    function mint(uint256 _id, address[] calldata _to, uint256[] calldata _quantities) external onlyMinter {
+
+        for (uint256 i = 0; i < _to.length; ++i) {
+
+            address to = _to[i];
+            uint256 quantity = _quantities[i];
+
+            // Grant the items to the caller
+            balances[_id][to] = quantity.add(balances[_id][to]);
+
+            // Emit the Transfer/Mint event.
+            // the 0x0 source address implies a mint
+            // It will also provide the circulating supply info.
+            emit TransferSingle(msg.sender, address(0x0), to, _id, quantity);
+        }
+    }
+
+    /**
+        @notice Transfers `_value` amount of an `_id` from the `_from` address to the `_to` address specified (with safety call).
+        @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
+        MUST revert if `_to` is the zero address.
+        MUST revert if balance of holder for token `_id` is lower than the `_value` sent.
+        MUST revert on any other error.
+        MUST emit the `TransferSingle` event to reflect the balance change (see "Safe Transfer Rules" section of the standard).
+        After the above conditions are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call `onERC1155Received` on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+        @param _from    Source address
+        @param _to      Target address
+        @param _id      ID of the token type
+        @param _value   Transfer amount
+    */
+    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value) external {
+
+        require(_to != address(0x0), "_to must be non-zero.");
+        require(_from == msg.sender || isMinter(msg.sender), "Need operator approval for 3rd party transfers.");
+
+        // SafeMath will throw with insuficient funds _from
+        // or if _id is not valid (balance will be 0)
+        balances[_id][_from] = balances[_id][_from].sub(_value);
+        balances[_id][_to]   = _value.add(balances[_id][_to]);
+
+        // MUST emit event
+        emit TransferSingle(msg.sender, _from, _to, _id, _value);
+    }
+
+    /**
+        @notice Transfers `_values` amount(s) of `_ids` from the `_from` address to the `_to` address specified (with safety call).
+        @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
+        MUST revert if `_to` is the zero address.
+        MUST revert if length of `_ids` is not the same as length of `_values`.
+        MUST revert if any of the balance(s) of the holder(s) for token(s) in `_ids` is lower than the respective amount(s) in `_values` sent to the recipient.
+        MUST revert on any other error.
+        MUST emit `TransferSingle` or `TransferBatch` event(s) such that all the balance changes are reflected (see "Safe Transfer Rules" section of the standard).
+        Balance changes and events MUST follow the ordering of the arrays (_ids[0]/_values[0] before _ids[1]/_values[1], etc).
+        After the above conditions for the transfer(s) in the batch are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call the relevant `ERC1155TokenReceiver` hook(s) on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).
+        @param _from    Source address
+        @param _to      Target address
+        @param _ids     IDs of each token type (order and length must match _values array)
+        @param _values  Transfer amounts per token type (order and length must match _ids array)
+    */
+    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values) external {
+
+        // MUST Throw on errors
+        require(_to != address(0x0), "destination address must be non-zero.");
+        require(_ids.length == _values.length, "_ids and _values array lenght must match.");
+        require(_from == msg.sender || isMinter(msg.sender) == true, "Need operator approval for 3rd party transfers.");
+
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+
+            // SafeMath will throw with insuficient funds _from
+            // or if _id is not valid (balance will be 0)
+            balances[id][_from] = balances[id][_from].sub(value);
+            balances[id][_to]   = value.add(balances[id][_to]);
+        }
+
+        // Note: instead of the below batch versions of event and acceptance check you MAY have emitted a TransferSingle
+        // event and a subsequent call to _doSafeTransferAcceptanceCheck in above loop for each balance change instead.
+        // Or emitted a TransferSingle event for each in the loop and then the single _doSafeBatchTransferAcceptanceCheck below.
+        // However it is implemented the balance changes and events MUST match when a check (i.e. calling an external contract) is done.
+
+        // MUST emit event
+        emit TransferBatch(msg.sender, _from, _to, _ids, _values);
+    }
+
+    /**
+        @notice Get the balance of an account's Tokens.
+        @param _owner  The address of the token holder
+        @param _id     ID of the Token
+        @return        The _owner's balance of the Token type requested
+     */
+    function balanceOf(address _owner, uint256 _id) external view returns (uint256) {
+        // The balance of any account can be calculated from the Transfer events history.
+        // However, since we need to keep the balances to validate transfer request,
+        // there is no extra cost to also privide a querry function.
+        return balances[_id][_owner];
+    }
+
+
+    /**
+        @notice Get the balance of multiple account/token pairs
+        @param _owners The addresses of the token holders
+        @param _ids    ID of the Tokens
+        @return        The _owner's balance of the Token types requested (i.e. balance for each (owner, id) pair)
+     */
+    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _ids) external view returns (uint256[] memory) {
+
+        require(_owners.length == _ids.length);
+
+        uint256[] memory balances_ = new uint256[](_owners.length);
+
+        for (uint256 i = 0; i < _owners.length; ++i) {
+            balances_[i] = balances[_ids[i]][_owners[i]];
+        }
+
+        return balances_;
+    }
+}
--- contracts/OutlandsUnitStatus.sol
+++ contracts/OutlandsUnitStatus.sol
@@ -5,7 +5,7 @@ import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/roles/Min
 
 /**
 * @dev 
-* 
+*   Kovan - 0x246e9084e0a8572FDAc05d2029CDe383c54A830c
 */
 
 contract OutlandsUnitStatus is MinterRole{
@@ -16,7 +16,7 @@ contract OutlandsUnitStatus is MinterRole{
     //tracks Cooldown 
     mapping (uint256 => uint256) public cool;
     //tracks status - max of 12 status
-    mapping (uint256 => uint8[12]) public status;
+    mapping (uint256 => uint16[12]) public status;
     //core data 
     address payable admin;
 
@@ -38,33 +38,44 @@ contract OutlandsUnitStatus is MinterRole{
      * @dev Returns the XP of a hero 
      * Total and available  
      */
-    function activeXP(uint256 id) public view returns(uint256, uint256) {
-        return (totalXP[id], totalXP[id].sub(usedXP[id]));
+    function getXP(uint256[] memory ids) public view returns(uint256[] memory total, uint256[] memory available) {
+        uint256 n = ids.length;
+        require(n <= 32);
+        //set dynamic sizes 
+        total = new uint256[](n);
+        available = new uint256[](n);
+        //loop 
+        uint256 _id;
+        for(uint256 i = 0; i < n; i++){
+            _id = ids[i];
+            total[i] = totalXP[_id];
+            available[i] = totalXP[_id].sub(usedXP[_id]);
+        }
     }
 
     /**
      * @dev Returns the available XP of unit  
      */
-    function getAvailableXP(uint256[] ids) public view returns(uint256[] xp) {
-        uint256 l = ids.length;
-        require(l <= 32);
-        xp = new uint256[](l);
-        for(uint256 i = 0; i < l; i++){
-          xp[i] = totalXP[id[i]].sub(usedXP[id[i]]);
+    function getAvailableXP(uint256[] memory ids) public view returns(uint256[] memory xp) {
+        uint256 n = ids.length;
+        require(n <= 32);
+        //set dynamic sizes 
+        xp = new uint256[](n);
+        for(uint256 i = 0; i < n; i++){
+          xp[i] = totalXP[ids[i]].sub(usedXP[ids[i]]);
         }
     }
     
     /**
      * @dev Gives a hero xp  
      */
-    function _giveXP(uint256 id, uint256 xp) internal {
-        totalXP[id] = totalXP[id].add(xp);
-    }
     function giveXP(uint256[] memory ids, uint256[] memory xp) public onlyMinter {
-        require(ids.length == xp.length);
         uint256 n = ids.length;
+        require(n == xp.length);
+        uint256 _id;
         for(uint256 i = 0; i < n; i++){
-            _giveXP(ids[i], xp[i]);
+            _id = ids[i];
+            totalXP[_id] = totalXP[_id].add(xp[i]);
         }
     }
     
@@ -72,24 +83,25 @@ contract OutlandsUnitStatus is MinterRole{
      * @dev Uses a heroe's xp
      */
     function useXP(uint256[] memory ids, uint256[] memory xp) public onlyMinter {
-        require(ids.length == xp.length);
         uint256 n = ids.length;
+        require(ids.length == xp.length);
         uint256 _id;
         for(uint256 i = 0; i < n; i++){
             _id = ids[i];
-            usedXP[_id] = usedXP[_id].add(xp[_id]);
+            usedXP[_id] = usedXP[_id].add(xp[i]);
         }
     }
 
     /**
      * @dev Returns the cool unit  
      */
-    function getCool(uint256[] ids) public view returns(uint256[] uCool) {
-        uint256 l = ids.length;
-        require(l <= 32);
-        uCool = new uint256[](l);
-        for(uint256 i = 0; i < l; i++){
-          uCool[i] = cool[ids[i]];
+    function getCool(uint256[] memory ids) public view returns(uint256[] memory cCool) {
+        uint256 n = ids.length;
+        require(n <= 32);
+        //set dynamic sizes 
+        cCool = new uint256[](n);
+        for(uint256 i = 0; i < n; i++){
+          cCool[i] = cool[ids[i]];
         }
     }
 
@@ -97,8 +109,8 @@ contract OutlandsUnitStatus is MinterRole{
      * @dev Gives a unit cool   
      */
     function setCool(uint256[] memory ids, uint256[] memory _cool) public onlyMinter {
-        require(ids.length == _cool.length);
         uint256 n = ids.length;
+        require(n == _cool.length);
         for(uint256 i = 0; i < n; i++){
             cool[ids[i]] = _cool[i];
         }
@@ -107,30 +119,31 @@ contract OutlandsUnitStatus is MinterRole{
     /**
      * @dev Returns the unit status  
      */
-    function getStatus(uint256[] ids) public view returns(uint8[][12] stat) {
-        uint256 l = ids.length;
-        require(l <= 32);
-        stat = new uint8[][12](l);
-        for(uint256 i = 0; i < l; i++){
-          stat[i] = status[ids[i]];
+    function getStatus(uint256[] memory ids) public view returns(uint16[12][] memory cStatus) {
+        uint256 n = ids.length;
+        require(n <= 32);
+        //set dynamic sizes 
+        cStatus = new uint16[12][](n);
+        for(uint256 i = 0; i < n; i++){
+          cStatus[i] = status[ids[i]];
         }
     }
 
     /**
      * @dev Sets a Unit status    
      */
-    function setStatus(uint256[] memory ids, uint8[] memory i, uint8[] memory stat) public onlyMinter {
-        require(ids.length == i.length && ids.length == stat.length);
+    function setStatus(uint256[] memory ids, uint8[] memory i, uint16[] memory _status) public onlyMinter {
         uint256 n = ids.length;
+        require(n == i.length && n == _status.length);
         for(uint256 j = 0; j < n; j++){
-            status[ids[j]][i[j]] = stat[j];
+            status[ids[j]][i[j]] = _status[j];
         }
     }
 
     /**
      * @dev Returns the data of a unit   
      */
-    function getUnitData(uint256[] ids) public view returns(uint256[] xp, uint256[] cCool, uint8[][12] cStatus) {
+    function getUnitData(uint256[] memory ids) public view returns(uint256[] memory xp, uint256[] memory cCool, uint16[12][] memory cStatus) {
         return (getAvailableXP(ids), getCool(ids), getStatus(ids));
     }
 }
